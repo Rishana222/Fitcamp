3bf23c5e7923e39510bce0e04d502455ddf73e3d
Rishana222 <rishanaponneth@gmail.com>
2025-10-15 12:52:48 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <>
       <img className='object-cover w-full   h-[900px] bg-top relative' src={Header} alt="" />
-      <div className='absolute top-3 '>
+      <div className='absolute top-3 lg:top-14  '>
         <div className='flex px-4 md:px-8 lg:px-24  '>
           <img src={Logo} alt="" className='w-10 md:w-auto  ' />
           <h1 className='ml-2 font-bold uppercase mt-2 md:text-2xl lg:text-3xl lg:mt-1'>fitcamp</h1>
@@ -18,7 +18,7 @@ const Navbar = () => {
           <h1>blog</h1>
           <h1>Testimonial</h1>
           <h1>About</h1>
-          <button className='mt-2 bg-indigo-500 text-white py-2  rounded-4xl px-2 relative top-[-6px] hover:bg-indigo-600 capitalize '>my subscribtion</button>
+          <button className='mt-2 lg:-mt-1 bg-indigo-500 text-white py-2  rounded-4xl px-2 relative top-[-6px] hover:bg-indigo-600 capitalize '>my subscribtion</button>
 
         </div>
       </div>
@@ -28,9 +28,16 @@ const Navbar = () => {
           <h1 className='capitalize text-xs mt-2'>over 100k+ members joined</h1>
         </div>
       </div>
-      <div> <h1 className='text-white capitalize lg:text-6xl lg:font-bold md:text-5xl md:font-medium  absolute left-1/2 transform -translate-x-1/2 top-[290px] text-xl font-medium sm:text-4xl sm:font-bold lg:top-[]'>priotize your health</h1></div>
-      <div className='absolute left-1/2 transform -translate-x-1/2 top-[350px] md:top-[380px] lg:top-[400px]  capitalize text-xs text-white sm:text-xl '>
+      <div> <h1 className='text-white capitalize lg:text-6xl lg:font-bold md:text-5xl md:font-medium  absolute left-1/2 transform -translate-x-1/2 top-[290px] text-xl font-medium sm:text-4xl sm:font-bold sm:top-[250px]'>priotize your health</h1></div>
+      <div className='absolute left-1/2 transform -translate-x-1/2 top-[350px] md:top-[350px] lg:top-[365px]  capitalize text-xs text-white sm:text-xl '>
         <p>transform your investing your wellness</p>
+      </div>
+      <div className='absolute left-1/2 transform -translate-x-1/2 top-[400px] bg-white text-black px-2 py-2 rounded-3xl w-[250px] text-xs lg:top-[400px]  flex'>
+        <input className='w-full ' type="search" placeholder="search gym location nearby" id="" />
+        <button className='bg-black text-white px-3 py-1.5 rounded-2xl '>search</button>
+      </div>
+      <div className='absolute bottom-0.5 bg-white text-black'>
+          yhye
       </div>
 
     </>
